1926f42550e42dc0e59b74e11ffb503037fc5b15
bkproworld <bkrv7799@gmail.com>
2023-07-12 10:15:13 +0530

    1stCommit

---
+++ JenkinsPractice/src/test/java/practiceJenkins/DemoTestJenkins.java
@@ -0,0 +1,12 @@
+package practiceJenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoTestJenkins {
+	
+	@Test
+	public void jenm1() {
+		System.out.println("This class is for test Jenkins");
+	}
+
+}
